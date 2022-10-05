eacc51f5d7e15c0d047da9ddca1f443a0c6753b5
Miguel Sedillo <sedillomiguel.des@gmail.com>
2022-10-04 18:52:37 -0700

    rehaul js, add mobile design for project pages, set alt tags for images

--- rehaul.js
+++ rehaul.js
@@ -23,93 +23,179 @@
 
 	processToggle.addEventListener('click', function () {
 		if (process.className == 'closed') {
-			process.className = 'open';
-			processDrop.style.display = 'flex';
-			design.style.display = `none`;
-			info.style.display = `none`;
-			design.style.opacity = `0`;
-			info.style.opacity = `0`;
-			setTimeout(function () {
-				document.getElementById('processArrow').style.transform = 'rotate(90deg)';
-				processDrop.style.opacity = '100';
-				processDrop.style.transform = `translateY(${processToggle.offsetHeight}px)`;
-			}, 10);
-			// calibrate();
+			if (window.innerWidth <= 450) {
+				process.className = 'open';
+				processDrop.style.display = 'flex';
+				design.style.display = `none`;
+				info.style.display = `none`;
+				design.style.opacity = `0`;
+				info.style.opacity = `0`;
+				setTimeout(function () {
+					document.getElementById('processArrow').style.transform = 'rotate(90deg)';
+					processDrop.style.opacity = '100';
+					processDrop.style.marginTop = `0px`;
+				}, 10);
+			} else {
+				process.className = 'open';
+				processDrop.style.display = 'flex';
+				design.style.display = `none`;
+				info.style.display = `none`;
+				design.style.opacity = `0`;
+				info.style.opacity = `0`;
+				setTimeout(function () {
+					document.getElementById('processArrow').style.transform = 'rotate(90deg)';
+					processDrop.style.opacity = '100';
+					processDrop.style.transform = `translateY(${processToggle.offsetHeight}px)`;
+				}, 10);
+			}
 		} else if (process.className == 'open') {
-			process.className = 'closed';
-			document.getElementById('processArrow').style.transform = 'rotate(45deg)';
-			processDrop.style.opacity = '0';
-			processDrop.style.transform = 'translateY(0px)';
-			design.style.display = `initial`;
-			info.style.display = `initial`;
-
-			setTimeout(function () {
-				processDrop.style.display = 'none';
-				design.style.opacity = `100`;
-				info.style.opacity = `100`;
-			}, 500);
+			if (window.innerWidth <= 450) {
+				process.className = 'closed';
+				document.getElementById('processArrow').style.transform = 'rotate(45deg)';
+				processDrop.style.opacity = '0';
+				processDrop.style.marginTop = '-128px';
+				design.style.display = `initial`;
+				info.style.display = `initial`;
+				setTimeout(function () {
+					processDrop.style.display = 'none';
+					design.style.opacity = `100`;
+					info.style.opacity = `100`;
+				}, 500);
+			} else {
+				process.className = 'closed';
+				document.getElementById('processArrow').style.transform = 'rotate(45deg)';
+				processDrop.style.opacity = '0';
+				processDrop.style.transform = 'translateY(0px)';
+				design.style.display = `initial`;
+				info.style.display = `initial`;
+				setTimeout(function () {
+					processDrop.style.display = 'none';
+					design.style.opacity = `100`;
+					info.style.opacity = `100`;
+				}, 500);
+			}
 		}
 	});
 
 	designToggle.addEventListener('click', function () {
 		if (design.className == 'closed') {
-			design.className = 'open';
-			designDrop.style.display = 'flex';
-			// calibrate();
-			process.style.display = 'none';
-			info.style.display = `none`;
-			process.style.opacity = `0`;
-			info.style.opacity = `0`;
-			setTimeout(function () {
-				document.getElementById('designArrow').style.transform = 'rotate(-90deg)';
-				designDrop.style.opacity = '100';
-				designDrop.style.transform = `translateY(${designToggle.offsetHeight}px)`;
-			}, 10);
+			if (window.innerWidth <= 450) {
+				design.className = 'open';
+				designDrop.style.display = 'flex';
+				// calibrate();
+				process.style.display = 'none';
+				info.style.display = `none`;
+				process.style.opacity = `0`;
+				info.style.opacity = `0`;
+				setTimeout(function () {
+					document.getElementById('designArrow').style.transform = 'rotate(-90deg)';
+					designDrop.style.opacity = '100';
+					designDrop.style.marginTop = `0px`;
+				}, 10);
+			} else {
+				design.className = 'open';
+				designDrop.style.display = 'flex';
+				// calibrate();
+				process.style.display = 'none';
+				info.style.display = `none`;
+				process.style.opacity = `0`;
+				info.style.opacity = `0`;
+				setTimeout(function () {
+					document.getElementById('designArrow').style.transform = 'rotate(-90deg)';
+					designDrop.style.opacity = '100';
+					designDrop.style.transform = `translateY(${designToggle.offsetHeight}px)`;
+				}, 10);
+			}
 		} else if (design.className == 'open') {
-			design.className = 'closed';
-			document.getElementById('designArrow').style.transform = 'rotate(-45deg)';
-			designDrop.style.opacity = '0';
-			designDrop.style.transform = 'translateY(0px)';
-			process.style.display = `initial`;
-			info.style.display = `initial`;
+			if (window.innerWidth <= 450) {
+				design.className = 'closed';
+				document.getElementById('designArrow').style.transform = 'rotate(-45deg)';
+				designDrop.style.opacity = '0';
+				designDrop.style.marginTop = '-128px';
+				process.style.display = `initial`;
+				info.style.display = `initial`;
+				setTimeout(function () {
+					designDrop.style.display = 'none';
+					process.style.opacity = `100`;
+					info.style.opacity = `100`;
+				}, 500);
+			} else {
+				design.className = 'closed';
+				document.getElementById('designArrow').style.transform = 'rotate(-45deg)';
+				designDrop.style.opacity = '0';
+				designDrop.style.transform = 'translateY(0px)';
+				process.style.display = `initial`;
+				info.style.display = `initial`;
 
-			setTimeout(function () {
-				designDrop.style.display = 'none';
-				process.style.opacity = `100`;
-				info.style.opacity = `100`;
-			}, 500);
+				setTimeout(function () {
+					designDrop.style.display = 'none';
+					process.style.opacity = `100`;
+					info.style.opacity = `100`;
+				}, 500);
+			}
 		}
 	});
 
 	infoToggle.addEventListener('click', function () {
 		if (info.className == 'closed') {
-			info.className = 'open';
-			infoDrop.style.display = 'flex';
-			// calibrate();
-			process.style.display = 'none';
-			design.style.display = `none`;
-			process.style.opacity = `0`;
-			design.style.opacity = `0`;
-			setTimeout(function () {
-				document.getElementById('infoArrow').style.transform = 'rotate(90deg)';
-				infoDrop.style.opacity = '100';
-				infoDrop.style.transform = `translateY(${infoToggle.offsetHeight}px)`;
-			}, 10);
+			if (window.innerWidth <= 450) {
+				info.className = 'open';
+				infoDrop.style.display = 'flex';
+				// calibrate();
+				process.style.display = 'none';
+				design.style.display = `none`;
+				process.style.opacity = `0`;
+				design.style.opacity = `0`;
+				setTimeout(function () {
+					document.getElementById('infoArrow').style.transform = 'rotate(90deg)';
+					infoDrop.style.opacity = '100';
+					infoDrop.style.marginTop = `0px`;
+				}, 10);
+			} else {
+				info.className = 'open';
+				infoDrop.style.display = 'flex';
+				// calibrate();
+				process.style.display = 'none';
+				design.style.display = `none`;
+				process.style.opacity = `0`;
+				design.style.opacity = `0`;
+				setTimeout(function () {
+					document.getElementById('infoArrow').style.transform = 'rotate(90deg)';
+					infoDrop.style.opacity = '100';
+					infoDrop.style.transform = `translateY(${infoToggle.offsetHeight}px)`;
+				}, 10);
+			}
 		} else if (info.className == 'open') {
-			info.className = 'closed';
-			document.getElementById('infoArrow').style.transform = 'rotate(45deg)';
-			infoDrop.style.opacity = '0';
-			infoDrop.style.transform = 'translateY(0px)';
+			if (window.innerWidth <= 450) {
+				info.className = 'closed';
+				document.getElementById('infoArrow').style.transform = 'rotate(45deg)';
+				infoDrop.style.opacity = '0';
+				infoDrop.style.marginTop = '-128px';
+				setTimeout(function () {
+					infoDrop.style.display = 'none';
+					process.style.display = `initial`;
+					design.style.display = `initial`;
+				}, 500);
+				setTimeout(function () {
+					process.style.opacity = `100`;
+					design.style.opacity = `100`;
+				}, 600);
+			} else {
+				info.className = 'closed';
+				document.getElementById('infoArrow').style.transform = 'rotate(45deg)';
+				infoDrop.style.opacity = '0';
+				infoDrop.style.transform = 'translateY(0px)';
 
-			setTimeout(function () {
-				infoDrop.style.display = 'none';
-				process.style.display = `initial`;
-				design.style.display = `initial`;
-			}, 500);
-			setTimeout(function () {
-				process.style.opacity = `100`;
-				design.style.opacity = `100`;
-			}, 600);
+				setTimeout(function () {
+					infoDrop.style.display = 'none';
+					process.style.display = `initial`;
+					design.style.display = `initial`;
+				}, 500);
+				setTimeout(function () {
+					process.style.opacity = `100`;
+					design.style.opacity = `100`;
+				}, 600);
+			}
 		}
 	});
 
